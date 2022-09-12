02fc95f3771925d29b5d4196030dbb413fcb1036
Valdecir Teixeira <vardeba@hotmail.com>
2022-09-12 20:35:03 -0300

    Entrega - S8-01 - Primeiro commit

--- js/database.js
+++ js/database.js
@@ -1,64 +1,50 @@
-// Banco de dados dos produtos
-
-const data = [
-  {
-    id: 1,
-    img: "../img/jaqueta.svg",
-    nameItem: "Lightweight Jacket",
-    description:
-      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
-  },
-  {
-    id: 2,
-    img: "../img/gorro.svg",
-    nameItem: "Black Hat",
-    description:
-      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
-  },
-  {
-    id: 3,
-    img: "../img/mascara.svg",
-    nameItem: "Mask",
-    description:
-      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-    value: 40,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
-  },
-  {
-    id: 4,
-    img: "../img/camiseta_preta.svg",
-    nameItem: "T-Shirt",
-    description:
-      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
-  },
-  {
-    id: 5,
-    img: "../img/camiseta_branca.svg",
-    nameItem: "Short-Sleeve T-Shirt",
-    description:
-      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
-  },
-  {
-    id: 6,
-    img: "../img/moletom.svg",
-    nameItem: "Champion Packable Jacket",
-    description:
-      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
-  },
-];
+let games = [
+    {
+        id: 1,
+        img: "../img/zelda_breath_01.webp",
+        nameGame: "The Legend of Zelda: Breath of the Wild",
+        description: "O novo jogo da famosa série que veio para romper barreiras.",
+        price: 299.00,
+        tag: ["Zelda"]
+    },
+    {
+        id: 2,
+        img: "../img/mario_odyssey_01.webp",
+        nameGame: "Super Mario Odyssey",
+        description: "Mario e Cappy em uma aventura 3D ao redor do mundo!",
+        price: 299.00,
+        tag: ["Mario"],
+    },
+    {
+        id: 3,
+        img: "../img/pokemon_arceus_01.webp",
+        nameGame: "Pokémon Legends: Arceus",
+        description: "Embarque em missões de pesquisa na antiga região de Hisui",
+        price: 299.00,
+        tag: ["Pokemon"],
+    },
+    {
+        id: 4,
+        img: "../img/zelda_link_awakening_01.webp",
+        nameGame: "The Legend of Zelda: Links Awakening",
+        description: "Link em sua aventura para despertar o legendário Wind Fish",
+        price: 299.00,
+        tag: ["Zelda"],
+    },
+    {
+        id: 5,
+        img: "../img/mario_maker_2_01.webp",
+        nameGame: "Super Mario Maker 2",
+        description: "Fãs do Mario, criem e compertilhem os seus próprios percursos!",
+        price: 299.00,
+        tag: ["Mario"],
+    },
+    {
+        id: 6,
+        img: "../img/zelda_skyward_01.webp",
+        nameGame: "The Legend of Zelda: Skyward Sword HD",
+        description: "Desça ao perigoso mundo da superfície nessa missão de alto voo pelos céus.",
+        price: 299.00,
+        tag: ["Zelda"],
+    }
+]
---
+++ js/index.js
@@ -0,0 +1,10 @@
+let produto = [
+    {
+        id: 1,
+        img: "../img/zelda_breath_01.webp",
+        nameGame: "The Legend of Zelda: Breath of the Wild",
+        description: "O novo jogo da famosa série que veio para romper barreiras.",
+        price: 299.00,
+        tag: ["Zelda"]
+    }
+];
